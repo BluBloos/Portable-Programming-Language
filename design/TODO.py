cf7e3a4e8d80767492136a62f31132d7ce4e5f46
BluBloos <cnoah1705@gmail.com>
2021-12-28 19:19:02 -0500

    Directory Organization, Example Programs

---
+++ design/TODO.py
@@ -0,0 +1,50 @@
+'''
+Need a language that is a superset to C.
+
+Targets:
+- Unity
+- Unreal Engine
+- Writing code for the web
+- IOS App
+- Android App
+- Apple Watch
+- Garmin Watches
+- Operating Systems
+    Windows, macOS, Linux
+- Writing code for the Cloud
+- Augmented Reality Experiences (OpenXR?)
+- Writing code to run on Graphics Cards
+- Binary, and for different architectures (for making Operating Systems)
+- Arduino
+
+Things to include in Standard Library:
+- Blockchain
+- Distributed Compute
+- Bluetooth
+- Wifi
+
+PPL should also be able to insert code for running Javascript as well as Python interpreters.
+Just be able to write these languages inline.
+I also believe that it would be very nice to write assembly inline.
+Maybe even support Go? I have never used Go...
+
+There should exist a custom language server and appropriate Visual Studio Code extensions for this language.
+
+Language should have a good profiler, a good debugger, and a good editor.
+When we think about a good editor, we think about virtual whitespace + DEXTER copilot.
+
+Potentially a drag and drop interface for creating GUIs.
+
+Novel Target Platform:
+- Custom web server
+    - Serves and receives novel content models
+        - Stream Video, Audio, User Input. Augmented reality experiences.
+        - Think "Writing a program and sharing it in seconds"
+            - Like, all other targets are transimissble...?
+- Custom web client
+
+Finally, think about device schronicity. Like the kind of ecosystem that Apple has, but we make this extremely cross-device.
+
+When you include something from the standard library
+
+'''
