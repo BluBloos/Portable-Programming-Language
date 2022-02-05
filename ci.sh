430d865720d90e76be1492f69219d720d154a066
BluBloos <cnoah1705@gmail.com>
2022-02-05 06:57:51 -0500

    File clean

--- ci.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Compile and run Build
-# NOTE(Noah): $@ is an array of all arguments passed on to build.sh
-g++ -std=c++11 -Wno-writable-strings -o bin/ci_linter -g util/ci_linter.c -I src/ -I util/ \
-	&& bin/ci_linter "$@"
--- src/legacy/ported/timing.py
+++
@@ -1,9 +0,0 @@
-import time
-
-class Timer:
-    def __init__(self):
-        self.timer_start = time.time()
-    def TimerEnd(self, msg, logger):
-        timer_end = time.time()
-        time_elapsed_in_ms = round((timer_end - self.timer_start) * 1000, 2)
-        logger.Log("Total time for {} = ".format(msg) + str( time_elapsed_in_ms ) + " ms" )
