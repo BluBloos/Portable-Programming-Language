4ce70190fc7215b7956cdf430f9926bb62bf6854
BluBloos <cnoah1705@gmail.com>
2022-03-14 06:08:40 -0400

    Added backend test to Windows CI

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+IF "%GITHUB_ACTIONS%"=="" call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 
 mkdir bin
 
