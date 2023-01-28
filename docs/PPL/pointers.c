3b199b287c05e105a6451a13a0156feed3a0e6e0
BluBloos <cnoah1705@gmail.com>
2023-01-28 17:42:26 -0500

    land on something for how to do pointers

--- docs/PPL/pointers.c
+++ docs/PPL/pointers.c
@@ -1,7 +1,8 @@
+// clang-format off
 // POINTERS.
 
 // NOTE: int is an alias for int32_t, always.
-->int a; 
+->int a;
 
 // issue with -> syntax is that a double pointer, or even a triple pointer becomes
 // gross. of course, -> increases readability (maybe).
@@ -18,7 +19,7 @@
 // so it should be okay for us to overlap with a binary operator.
 
 // Pascal:
-int x;
+int  x;
 ^int a;
 a = @x;
 ^a = 3;
@@ -28,12 +29,54 @@ a = @x;
 // makes it easy to read code.
 // as opposed to allowing the user to use
 // whatever they want. Keep it simple.
+//
+// in pondering > vs ^, ^ is at least consistent visually with * as these take
+// up the upper half of the space allocated for a single character.
 
-// Decision #2: i'd actually like to go with >
-// for the reason that deref can be the opposite,
-// with <.
+// Here's another question. On which side of the type do we put the pointer syntax?
+^^int vs. int^^ ? ;
 
-int32_t main(int32_t argc, >>uint8_t argv)
-{
-    
+// since we are borrowing from Pascal, we ought to stick with how it does things.
+^^int; // is correct.
+
+// Question:
+// How are we meant to do the two const ideas?
+// Where the memory pointed to may not be modified versus the pointer itself cannot be modded?
+
+// HOW QUALIFIERS WORK:
+// maybe say that qualifier modifes strictly what is to the right.
+// it only modifies just one item.
+// a qualifier cannot modify a qualifer, so it searches until it can find the first non qualifer. this allows for qualifier sequences.
+
+// thus,
+const int A; 
+// modifies direct to its right.
+// this becomes, "declare a constant integer called A".
+// const sort of folds into the value type.
+
+int const A; // redundant. the identifier A (UID part of UID<T>) is not going to change.
+// still allowable but again redundant.
+// (unless we are dealing with references).
+
+// if we wanted duck stuff (which I do not see why),
+// maybe we use some diff syntax.. so for now, don't care about that.
+
+// so if ...
+const ^int A;
+// "declare a constant pointer to an integer called A".
+
+// then
+^const int A;
+// "declaree a pointer to a constant integer called A".
+
+
+// also, there may be an implicit let,
+let const int A;
+// "let there be a consant integer called A".
+// where the "let there be ..." is an alloc/reserve idea.
+
+// we have to remeember that these decls create a VARIABLE.
+// which is some location and an identifier desribing/identifying it.
+
+int32_t main(int32_t argc, ^^uint8_t argv) {
 }
