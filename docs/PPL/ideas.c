a4be20cf3e219e0166470fea1b4689ba23436d12
BluBloos <cnoah1705@gmail.com>
2023-01-28 17:53:33 -0500

    ideas

---
+++ docs/PPL/ideas.c
@@ -0,0 +1,23 @@
+/*
+The answer should be, "what is the natural way that you would type it".
+and this is derived from the order of thought.
+
+When you are writing code, and you think about "I want to make a variable of type
+integer with a name of A".
+Hence,
+int A;
+Over,
+A: int;
+*/
+
+// One idea is the following : could create an IDE that virtualizes the syntax into your
+// preferred style. According to how you formulate sentences in your head.
+// give user a quiz when they first begin the IDE. A series of short questions where they say,
+// "I prefer this over that".
+
+// #idea, what if also we could identify a variable to a location on some IO module
+// that is not memory mapped?
+// or a location on the disk drive?
+//
+// this idea _may_ be much too high-level ...
+// but interesting nonetheless :D
