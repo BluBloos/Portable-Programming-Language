dd4bcfc1a37cc37c4d3b4cd963956218247fcd7c
BluBloos <cnoah1705@gmail.com>
2023-01-30 22:31:12 -0500

    formatting dix

--- docs/PPL/attributes.cpp
+++ docs/PPL/attributes.cpp
@@ -3,10 +3,9 @@ int f() {
     return 0;
 }
 
-// we use the pack attribute to ensure that the compiler will pack this struct in memory, ignoring the 
-// performance implications of this.
-[[pack]]
-struct S {
-    int a;
+// we use the pack attribute to ensure that the compiler will pack this struct
+// in memory, ignoring the performance implications of this.
+[[pack]] struct S {
+    int  a;
     char b;
 };
