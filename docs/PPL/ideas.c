4f3ff4491a96559e9c639156a063c6cc45307883
BluBloos <cnoah1705@gmail.com>
2023-04-18 07:28:43 -0400

    Docs: get rid of stupid idea.

--- docs/PPL/ideas.c
+++ docs/PPL/ideas.c
@@ -1,15 +1,3 @@
-/*
-When you are writing code, and you think about "I want to make a variable of
-type integer with a name of A". Hence, int A; Over, A: int;
-
-At least, if you are a C programmer you would think this!
-*/
-
-// One idea is the following : could create an IDE that virtualizes the syntax
-// into your preferred style. Preferred style = how you formulate sentences in
-// your head. Give user a quiz when they first begin the IDE. A series of short
-// questions where they say, "I prefer this over that".
-
 // #idea, what if also we could identify a variable to a location that is not
 // stack or heap? this idea _may_ be much too high-level ... but interesting
 // nonetheless :D
