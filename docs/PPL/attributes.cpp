5995d1b2fa567866582e1e06a4a228fd31ec0fed
BluBloos <cnoah1705@gmail.com>
2023-04-17 21:50:19 -0400

    Docs: remove nodiscard.

--- docs/PPL/attributes.cpp
+++ docs/PPL/attributes.cpp
@@ -1,8 +1,3 @@
-[[nodiscard]] // same as nodiscard in C++17
-int f() {
-    return 0;
-}
-
 // we use the pack attribute to ensure that the compiler will pack this struct
 // in memory, ignoring the performance implications of this.
 [[pack]] struct S {
