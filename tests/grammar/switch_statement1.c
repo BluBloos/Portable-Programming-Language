8a531b9bc19ad5afb39fd0ea664c176047491289
BluBloos <cnoah1705@gmail.com>
2023-05-04 07:19:41 -0400

    Add more grammar tests.

---
+++ tests/grammar/switch_statement1.c
@@ -0,0 +1,12 @@
+/* 
+All poopoo are an opportunity to peepee, but not all 
+peepee are an opportunity to poopoo
+    - Talib
+*/
+
+switch myStr {
+    case "poopoo":
+        printf("Oh lord he poopin'");
+    case "peepee":
+        printf("Is he poopin' or peein'? We dunno...");
+}
