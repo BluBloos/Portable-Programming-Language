69e996cc8da066be07f691a92f4077bbafa3d31a
BluBloos <cnoah1705@gmail.com>
2024-02-06 13:05:15 -0500

    build.sh no longer launches the compiler; it just builds.

--- build.sh
+++ build.sh
@@ -6,5 +6,4 @@ mkdir bin
 # Compile and run Build
 # NOTE(Noah): $@ is an array of all arguments passed on to build.sh
 g++ -std=c++11 -Wall -Wno-unused-value -Wno-write-strings -Wno-writable-strings -o bin/ppl \
-	-g util/build.cpp -I tests/ -I src/ -I util/ -I vendor/ \
-	&& bin/ppl "$@"
+	-g util/build.cpp -I tests/ -I src/ -I util/ -I vendor/
